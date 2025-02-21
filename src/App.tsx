b188e9bb0c4f1012d84651f574a961600c90949e
udirose <udirose@wustl.edu>
2025-02-21 13:52:44 -0600

    configure correctly for ghp

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,25 @@ import About from './components/About';
 import Projects from './components/projects/Projects';
 import Contact from './components/Contact';
 import itch from './images/itch.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 
 const App: React.FC = () => {
     return (
-        <div>
-            <header>
-                <h1>UDR GAMES</h1><a href="https:udro.itch.io" target="_blank" rel="noopener noreferrer">
-                    <img src={itch} alt="itch" style={{ width: '200px', height: 'auto' }} />
-                </a>
-            </header>
-            <main>
-                <Projects />
-                <About />
-                <Contact />
-            </main>
-        </div>
+        <Router basename="/udr">
+            <div>
+                <header>
+                    <h1>UDR GAMES</h1><a href="https:udro.itch.io" target="_blank" rel="noopener noreferrer">
+                        <img src={itch} alt="itch" style={{ width: '200px', height: 'auto' }} />
+                    </a>
+                </header>
+                <main>
+                    <Projects />
+                    <About />
+                    <Contact />
+                </main>
+            </div>
+        </Router>
     );
 }
 
