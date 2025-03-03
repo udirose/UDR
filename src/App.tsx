98505660a85786cf0834ac50ce64506ad0786e38
udirose <udirose@wustl.edu>
2025-03-03 14:29:44 -0600

    center align

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import Projects from './components/projects/Projects';
 import Contact from './components/Contact';
 import itch from './images/itch.svg';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import './App.css';
 
 const App: React.FC = () => {
     return (
         <Router basename="/">
             <div>
                 <header>
-                    <h1>UDR GAMES</h1><a href="https://udro.itch.io" target="_blank" rel="noopener noreferrer">
-                        <img src={itch} alt="itch" style={{ width: '200px', height: 'auto' }} />
+                    <h1>UDR GAMES</h1>
+                    <a href="https://udro.itch.io" target="_blank" rel="noopener noreferrer">
+                        <img src={itch} alt="itch" style={{ width: '180px', height: 'auto' }} />
                     </a>
                 </header>
                 <main>
